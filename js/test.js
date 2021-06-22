39b3318e813b158820e02adac5da64d30a6e467a
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2021-06-21 17:48:27 -0700

    changed logic of magic8ball

---
+++ js/test.js
@@ -0,0 +1,46 @@
+const input = document.querySelector("#questionArea");
+const button = document.querySelector("#ask");
+const reset = document.querySelector("#reset");
+const answer = document.querySelector("#answer");
+const answerArray = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+];
+
+button.addEventListener("click", displayImage);
+reset.addEventListener("click", resetButton);
+
+function generateRandomNum(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function resetButton() {
+  input.value = "";
+  answer.innerHTML = "Ask Again Later";
+}
+
+function displayImage() {
+  if (input.value == "") {
+    alert("Enter a question...");
+    return;
+  }
+  answer.innerHTML = answerArray[generateRandomNum(20)];
+}
